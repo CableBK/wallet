aa9975349663e80e365c75dce68d2c0fd32bc729
CableBK <dev.witchapat@gmail.com>
2025-03-30 21:54:44 +0700

    add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:wallet/pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,48 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FluentApp(
-      title: 'Fluent Navigation View Example',
-      theme: FluentThemeData(brightness: Brightness.light),
-      home: NavigationViewExample(),
-    );
-  }
-}
-
-class NavigationViewExample extends StatefulWidget {
-  @override
-  _NavigationViewExampleState createState() => _NavigationViewExampleState();
-}
-
-class _NavigationViewExampleState extends State<NavigationViewExample> {
-  int selectedIndex = 0;
-
-  final List<String> pages = ['Home Page', 'Settings Page', 'Profile Page'];
-
-  @override
-  Widget build(BuildContext context) {
-    return ScaffoldPage(
-      header: const PageHeader(title: Text('Fluent Navigation View')),
-      content: NavigationView(
-        appBar: NavigationAppBar(title: Text(pages[selectedIndex])),
-        pane: NavigationPane(
-          selected: selectedIndex,
-          onChanged: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-          items: [
-            PaneItemHeader(header: const Text('Main Menu')),
-            PaneItem(icon: Icon(FluentIcons.home), title: Text('Home')),
-            PaneItem(icon: Icon(FluentIcons.settings), title: Text('Settings')),
-            PaneItem(icon: Icon(FluentIcons.contact), title: Text('Profile')),
-          ],
-        ),
-        content: Center(
-          child: Text(pages[selectedIndex], style: TextStyle(fontSize: 24)),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'wallet', home: App());
   }
 }
